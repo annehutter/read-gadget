661b87c3f21a2ad64fc54abbf23fec5243ca7de2
ahutter <anne.k.hutter@gmail.com>
2016-01-13 12:11:01 +1100

    included SPH density calculation

--- sph_density.c
+++ sph_density.c
@@ -76,23 +76,10 @@ static float dist_sq( float *a1, float *a2, int dims ) {
 
 void compute_SPH_density(domain_t *thisDomain, header_t *thisHeader, part_t *theseParticles)
 {
-// 	float xp, yp, zp;
-// 	float xp2, yp2, zp2;
-// 	
 	float distance, dist_sq;
 	float h;
-// 	float density;
-// 	float *rho;
-	
-// 	int *gridcells;
-// 	int nbins = 128;
-
-	
-// 	float boxsize = thisHeader->BoxSize;
-// 	float inv_boxsize = 1.f/boxsize;
 	
 	theseParticles->rho = malloc(sizeof(float)*theseParticles->num);
-// 	gridcells = malloc(sizeof(int)*theseParticles->num);	
 	
 	void *kd = kd_create(3);
 	
@@ -124,114 +111,8 @@ void compute_SPH_density(domain_t *thisDomain, header_t *thisHeader, part_t *the
 		}
 		kd_res_free(res);
 	}
-// 	printf("found %d results:\n", kd_res_size(res));
-// 	while( !kd_res_end( res ) ) 
-// 	{
-// 		/* get the data and position of the current result item */
-// 		pch = (char*)kd_res_itemf( res, pos );
-// 
-// 		/* compute the distance of the current result from the pt */
-// 		dist = sqrt( dist_sq( &theseParticles->pos[0], pos, 3 ) );
-// 
-// 		/* print out the retrieved data */
-// 		printf( "node at (%.3f, %.3f, %.3f) is %.3f away\n", pos[0], pos[1], pos[2], dist);
-// 
-// 		/* go to the next entry */
-// 		kd_res_next( res );
-// 	}
+
 	MPI_Barrier(MPI_COMM_WORLD);
 	
 	kd_free(kd);
-// 	for(int p=0; p<theseParticles->num; p++)
-// 	{
-// 		if(p%1000 == 0) printf("rank %d: p = %d\t %f\n", thisDomain->originRank, p/1000, density);
-// 		rho[p] = 0.;
-// 		
-// 		xp = theseParticles->pos[3*p];
-// 		yp = theseParticles->pos[3*p+1];
-// 		zp = theseParticles->pos[3*p+2];
-// 		
-// 		gridcells[p] = xp*inv_boxsize*nbins*nbins+yp*inv_boxsize*nbins+zp*inv_boxsize;
-// 		density = 0.;
-// 		for(int p2=0; p2<theseParticles->num; p2++)
-// 		{
-// 			if(gridcells[p] == gridcells[p2])
-// 			{
-// 				xp2 = theseParticles->pos[3*p2];
-// 				yp2 = theseParticles->pos[3*p2+1];
-// 				zp2 = theseParticles->pos[3*p2+2];
-// 				
-// 				distance = xp2;
-// // 				distance = calc_distance(xp, yp, zp, xp2, yp2, zp2);
-// 				density += get_SPH_density(distance, "SPH");
-// 			}
-// 		}
-// // 		rho[p] = density;
-// 	}
-	
-	
-		
-// 	printf("start sorting\n");
-// 	for(int p=1; p<theseParticles->num; p++)
-// 	{
-// 		if(p%100000==0) printf("%d\n",p);
-// 		valueToSort = gridcells[sortedArray[p]];
-// 		valueToSort_index = sortedArray[p];
-// 		int p2 = p;
-// 		while(p2>0 && gridcells[sortedArray[p2-1]]>valueToSort)
-// 		{
-// 			sortedArray[p2] = sortedArray[p2-1];
-// 			p2 = p2-1;
-// 		}
-// 		sortedArray[p2] = valueToSort_index;
-// 	}
-// 	printf("end sorting\n");
-	
-// 	for(int p=0; p<theseParticles->num; p++) if(p<100 && thisDomain->originRank==0) printf("%d\n",sortedArray[p]);
-
-// 		range = 1;
-// 		for(int x=-range; x<=range; x++)
-// 		{
-// 			for(int y=-range; y<=range; y++)
-// 			{
-// 				for(int z=-range; z<=range; z++)
-// 				{
-// 					neighbour_cell = x*nbins*nbins+y*nbins+z;
-// 					for(int p2=0; p2<theseParticles->num; p2++)
-// 					{
-// 						if(p2 == neighbour_cell)
-// 						{
-// 							xp2 = theseParticles->pos[3*p2];
-// 							yp2 = theseParticles->pos[3*p2+1];
-// 							zp2 = theseParticles->pos[3*p2+2];
-// 							
-// 							distance = calc_distance(xp, yp, zp, xp2, yp2, zp2);
-// 							density = get_SPH_density(distance, "SPH");
-// 							
-// 							theseParticles->rho[p] += density;
-// 						}
-// 					}
-// 				}
-// 			}
-// 		}
-// 		for(int p2=0; p2<theseParticles->num; p2++)
-// 		{
-// 			if(p != p2)
-// 			{
-// 				xp2 = theseParticles->pos[3*p2];
-// 				yp2 = theseParticles->pos[3*p2+1];
-// 				zp2 = theseParticles->pos[3*p2+2];
-// 				
-// 				distance = calc_distance(xp, yp, zp, xp2, yp2, zp2);
-// 				if(distance<1.)
-// 				{
-// 					density = get_SPH_density(distance, "SPH");
-// 				
-// 					theseParticles->rho[p] += density;
-// 				}
-// 			}
-// 		}
-
-// 	free(rho);
-// 	free(gridcells);
 }
